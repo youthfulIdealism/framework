5d14fb54c4f7ed94f5087c55d9c462f9401f9aac
Samuel Richardson <groupofgremilins@gmail.com>
2025-09-26 07:19:39 -0700

    exported the thing I needed

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,5 @@
 export { F_Collection_Registry } from './F_Collection_Registry.js';
 export { F_Collection } from './F_Collection.js';
 export { compile } from './F_Compile.js';
-export { z_mongodb_id, z_mongodb_id_optional } from './utils/mongoose_from_zod.js';
+export { z_mongodb_id, z_mongodb_id_optional, z_mongodb_id_nullable } from './utils/mongoose_from_zod.js';
 export { pretty_print as pretty_print_zod } from './utils/pretty_print_zod.js';
